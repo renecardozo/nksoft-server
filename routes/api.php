8a61ef4acae82315edae387e2c194f0e571ed8ab
DanitzaSandoval <sandovalvillarroeld@gmail.com>
2024-04-15 15:38:04 -0400

    agregar materias server

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use APP\Http\Controllers\API\MateriaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1/materias')->group(function () {
+    Route::get('/',[ MateriaController::class, 'get']);
+    Route::post('/',[ MateriaController::class, 'store']);
+    Route::get('/{id}',[ MateriaController::class, 'getById']);
+    Route::put('/{id}',[ MateriaController::class, 'update']);
+    Route::delete('/{id}',[ MateriaController::class, 'delete']);
+});
+
+Route::get('/materias', 'App\Http\Controllers\API\MateriaController@index');
+Route::post('/materias', 'App\Http\Controllers\API\MateriaController@store');
+Route::put('/materias', 'App\Http\Controllers\API\MateriaController@update');
+Route::get('/materias/{id}', 'App\Http\Controllers\API\MateriaController@getById');
 
 Route::get('/docentes', 'App\Http\Controllers\DocenteController@index');
 Route::post('/docentes', 'App\Http\Controllers\DocenteController@store');
