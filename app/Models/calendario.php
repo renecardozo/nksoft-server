7ab526c0f720e57e8a426e03b5823519dbd216b2
pxlxth <poleth.tordoya@gmail.com>
2024-04-21 20:27:47 -0400

    version2.0

--- app/Models/calendario.php
+++ app/Models/calendario.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class calendario extends Model
+class Calendario extends Model
 {
     use HasFactory;
 }
