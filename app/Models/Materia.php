3ed37cebb111e6045c3ad2012f3530eced48e212
DanitzaSandoval <sandovalvillarroeld@gmail.com>
2024-05-17 19:35:44 -0400

    Docente eliminado

--- app/Models/Materia.php
+++ app/Models/Materia.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Materia extends Model
 {
     use HasFactory;
-    protected $fillable = ['id_materia','codigo', 'materia', 'grupo', 'docente', 'departamento'];
+   // protected $fillable = ['id_materia','codigo', 'materia', 'grupo', 'docente', 'departamento'];
+    protected $fillable = ['id_materia','codigo', 'materia', 'grupo', 'departamento'];
     protected $table = 'materia';
 }
